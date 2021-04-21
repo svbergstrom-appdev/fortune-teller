3b6501b87ed8041568f0ed41d4c826bce4e28731
svbergstrom <sbergstr@chicagobooth.edu>
2021-04-21 19:25:52 +0000

    finished earth signs

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,9 @@ Rails.application.routes.draw do
   get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
   get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
-  # get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
-  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
   # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
   # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
