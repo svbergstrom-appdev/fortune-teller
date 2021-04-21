8f00589a900d6468ee90e59ea8e8489ccc95f2b3
svbergstrom <sbergstr@chicagobooth.edu>
2021-04-21 19:11:22 +0000

    added unlucky number generator

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/boohoo.html.erb"})
+  end 
 end
